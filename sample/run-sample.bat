76c237b49d3348455ce54a4a528a5dd6fb90ba86
Claus Nagel <cnagel@virtualcitysystems.de>
2021-04-13 21:12:01 +0200

    bumping version to 2.9.1

--- sample/run-sample.bat
+++ sample/run-sample.bat
@@ -10,4 +10,4 @@ set PACKAGE="ade.sub.jaxb"
 set BINDING="binding.xjb"
 set SCHEMA="CityGML-SubsurfaceADE-0_9_0.xsd"
 
-call ..\bin\ade-xjc.bat -clean -output %OUTPUT% -package %PACKAGE% -binding %BINDING% %SCHEMA%
+call ..\ade-xjc.bat -clean -output %OUTPUT% -package %PACKAGE% -binding %BINDING% %SCHEMA%
